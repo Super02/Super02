b20c9fab79c8e23153348e8b66011b4c5f330acd
Super02 <34713084+Super02@users.noreply.github.com>
2020-10-03 16:05:57 +0200

    Update dab.py

--- dab.py
+++ dab.py
@@ -4,6 +4,7 @@ input_stripped = boof_input.strip()
 if input_stripped == 'dab':
 	print("DABBBBB")
 elif input_stripped == 'aab':
-	print("App")
+	print("App"*2)
 else:
 	print("And I oop")
+	print("And oof")
